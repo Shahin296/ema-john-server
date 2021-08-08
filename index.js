1158abf157e47935b82ef618881d01965881d838
shahin <mohammadshahin237@gmail.com>
2021-08-08 03:18:56 -0700

    added / route

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ app.use(bodyParser.urlencoded({extended: false}));
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.60vos.mongodb.net/emaJohnStore?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-
+app.get("/", (req, res)=>{
+    res.send("working on this route!")
+})
 
 client.connect(err => {
   const productCollection = client.db("emaJohnStore").collection("products");
